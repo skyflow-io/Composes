95a14d6bf9ccc3237ff719abb8eb4b38352434e3
nsbx <nsbx@hotmail.fr>
2019-05-20 18:34:35 +0200

    feat(ci): add dominant color function

--- ci/build.php
+++ ci/build.php
@@ -4,7 +4,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
-use \Colors\RandomColor;
+use ColorThief\ColorThief;
 
 $fileSystem = new Filesystem();
 $result = [];
@@ -30,10 +30,13 @@ foreach ($composeFinder as $compose) {
         ];
         if($filename === ($name . '.config.json')){
             $doc = json_decode($file->getContents(), true);
-            $doc['color'] = strtoupper(RandomColor::one());
-            $doc['logo'] = 'default.svg';
+            $doc['logo'] = 'default.png';
             if($fileSystem->exists('public/images/' . $doc['slug'] . '.png')){
                 $doc['logo'] = $doc['slug'] . '.png';
+            }
+            if(!$doc['color'] && $doc['logo']){
+                $color = ColorThief::getColor('public/images/' . $doc['logo']);
+                $doc['color'] = sprintf("#%02x%02x%02x", $color[0], $color[1], $color[2]);
             }
             $docResult[] = $doc;
         }
